b93c9d2f0dc967d3bb2b356546e9953fca1e2435
EKL20 <45775770+EKL20@users.noreply.github.com>
2019-12-26 14:47:11 -0500

    Se crean parametros, métodos y la clase principal del proyecto

--- modelo/Curso.java
+++ modelo/Curso.java
@@ -1,6 +1,13 @@
 package modelo;
 
 public class Curso {
-  
+    private String codigoCurso;
+    private String nombreCurso;
+    private int creditos;
+    private double nota;
+
+    public Curso(String cod, String nom, int cred){
+    }
+    
 
 }
--- modelo/Estudiante.java
+++ modelo/Estudiante.java
@@ -1,6 +1,40 @@
 package modelo;
 
 public class Estudiante {
+    private int codigo;
+    private String nombre;
+    private  String apellido;
 
+    public  Estudiante(String nombre, String apellido, int codigo){
+    }
+
+    public boolean asignarCurso(Curso curso){
+
+        return asignarCurso(curso);
+    }
+    public double calcularPromedio(){
+
+        return calcularPromedio();
+    }
+    public boolean pensumIncluyeCurso(String codigoCurso){
+
+
+        return false;
+    }
+    public boolean estudianteEstaPrubea(){
+
+
+        return false;
+    }
+    public boolean tieneCursosCompletos(String codigoCurso){
+
+        return false;
+    }
+    public Curso buscarCurso(String codigoCurso){
+        Curso curso;
+        curso = new Curso(String cod, String nom, int cred);
+
+        return curso;
+    }
 
 }
