03ac23cdfb5eaf1bfc104b168067b65bb3fe62e4
EKL20 <45775770+EKL20@users.noreply.github.com>
2019-12-26 16:40:11 -0500

    Se crea el registro de estudiantes

--- modelo/Estudiante.java
+++ modelo/Estudiante.java
@@ -1,12 +1,33 @@
 package modelo;
 
+import java.util.Scanner;
+
 public class Estudiante {
     private int codigo;
     private String nombre;
     private  String apellido;
 
+    String[] estudiante = new String[3];
+    Scanner ing = new Scanner(System.in);
     public  Estudiante(String nombre, String apellido, int codigo){
+
+        this.codigo= codigo;
+        this.nombre= nombre;
+        this.apellido= apellido;
+
+
+        estudiante[0]= nombre;
+        estudiante[1]= apellido;
+        estudiante[2]= Integer.toString(codigo);
+
+        for (String e :
+                estudiante) {
+            System.out.println(e);
+        }
+
+
     }
+
 
     public boolean asignarCurso(Curso curso){
 
@@ -30,12 +51,15 @@ public class Estudiante {
 
         return false;
     }
+    /*
     public Curso buscarCurso(String codigoCurso){
         Curso curso;
        // curso = new Curso(String cod, String nom, int cred);
 
         return curso;
     }
+
+     */
 
     public int getCodigo() {
         return codigo;
@@ -48,10 +72,10 @@ public class Estudiante {
     public String getApellido() {
         return apellido;
     }
-    public Curso getCurso1(){return Curso;}
+   /* public Curso getCurso1(){return Curso;}
     public Curso getCurso2(){return Curso;}
     public Curso getCurso3(){return Curso;}
     public Curso getCurso4(){return Curso;}
     public Curso getCurso5(){return Curso;}
-
+*/
 }
--- ppal/Principal.java
+++ ppal/Principal.java
@@ -1,8 +1,25 @@
 package ppal;
 
+import modelo.Estudiante;
+
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
-        
+
+        Estudiante Elkin;
+        Scanner ing = new Scanner(System.in);
+
+
+        System.out.println("Ingrese el nombre:");
+        String nombre = ing.next();
+        System.out.println("Ingrese el apellido:");
+        String apellido = ing.next();
+        System.out.println("Ingrese el codigo del estudiante:");
+       int codigo= ing.nextInt();
+
+        Elkin = new Estudiante(nombre, apellido,codigo);
+
     }
 }
